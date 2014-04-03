aa7b09aba8b01928f47de81123da9a6838cfc3d7
ajh5449 <ajh5449@psu.edu>
2014-04-03 15:52:24 -0400

    index.php
    homepage

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Penn State - e-Auction</title>
+
+<meta charset="utf-8">
+<meta name="author" content="Anthony Wano">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="431W Project"> 
+<link rel="stylesheet" href="431W.css">
+<link rel="stylesheet" href="431Wmobile.css" media="only screen and (max-device-width: 480px)">
+
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+
+</head>
+
+
+<body>
+
+<div id="wrapper">
+<h1>e-Auction</h1>
+
+    <div id="divMenu">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="login.html">LOGIN</a>
+                <ul>
+            <li><a href="RegularLogin.html">Regular Login</a></li>
+            <li><a href="AdminLogin.html">Admin Login</a></li>
+        </ul></li>
+        <li><a href="Categories.html">Categories</a>
+                <ul>
+            <li><a href="xxx.html">XXX</a></li>
+            <li><a href="yyy.html">YYY</a></li>
+            <li><a href="zzz.html">ZZZ</a></li>
+        </ul></li>
+        <li><a href="Contact.html">Contact Us</a>
+                <ul>
+            <li><a href="aaa.html">AAA</a></li>
+            <li><a href="bbb.html">BBB</a></li>
+        </ul></li>
+    </ul>
+    </div>
+
+
+<div id="content">
+<h2>Welcome to e-Auction!</h2>
+
+
+<form action="demo_form.asp">
+   SEARCH: <input type="text" name="search" size="85"><br>
+   <input type="submit" value="Submit">
+ </form>
+
+<div class="centeredImage">
+<img src="auction.jpg" alt="Project Logo" height="80%" width="80%" align="middle" padding-left:10%>
+</div>
+
+<div class="clear">
+</div>
+
+<br><br>
+<p>------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+<br><br>
+
+
+<div class="centeredImage">
+<img src="whatsHot.jpg" alt="Project Logo" height="80%" width="80%" align="middle" padding-left:10%>
+</div>
+
+<div class="clear">
+</div>
+
+<div class="clear">
+<div id="gallery">
+	<ul>
+		<li><a href="xbox-one.jpg"><img src="xbox-one.jpg" width="100" height="75" alt="xbox-one"><span><img src="xbox-one.jpg" width="300" height="225" alt="xbox-one"><br>Xbox One - $499.99</span></a></li>
+ 	
+		<li><a href="Google_Glass.jpg"><img src="Google_Glass.jpg" width="100" height="75" alt="Google_Glass"><span><img src="Google_Glass.jpg" width="300" height="225" alt="Google_Glass"><br>Google Glass - $600.00</span></a></li>
+ 
+ 		<li><a href="dodge-challenger.jpg"><img src="dodge-challenger.jpg" width="100" height="75" alt="dodge-challenger"><span><img src="dodge-challenger.jpg" width="300" height="225" alt="dodge-challenger"><br>Dodge Challenger - $45,000.00</span></a></li>
+
+		<li><a href="rocket.jpg"><img src="rocket.jpg" width="100" height="75" alt="rocket"><span><img src="rocket.jpg" width="300" height="225" alt="rocket"><br>Rocket - $1 MIL</span></a></li>
+	</ul>
+</div>
+</div>
+<div class="clear">
+</div>
+
+<br><br><br><br>
+<div id="footer">
+Copyright &copy; 2014 Anthony Wano<br>
+<a href="mailto:ajw5459@psu.edu">ajw5459@psu.edu</a><br><br>
+This page is designed for Pennsylvania State University CMPSC 431W. Web Designer: Anthony Wano.
+</div>
+
+</div>
+<br>
+
+</div>
+
+
+</body>
+</html>
